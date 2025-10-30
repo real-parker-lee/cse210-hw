1de43bd26c4a0147ad3b9316db8acba949782126
real-parker-lee <real-parker-lee@byui.edu>
2025-10-30 11:28:48 -0600

    Prep2: implement steps 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // prompt for and parse integer percentage
+        Console.Write("Enter the grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+        
+        // print appropriate grade
+        Console.Write("Your Letter Grade Is: ");
+        if (percentage >= 90) {
+            Console.WriteLine("A");
+        } else if (percentage >= 80) {
+            Console.WriteLine("B");
+        } else if (percentage >= 70) {
+            Console.WriteLine("C");
+        } else if (percentage >= 60) {
+            Console.WriteLine("D");
+        } else {
+            Console.WriteLine("F");
+        }
+        
+        // determine if user passed the course
+        if (percentage >= 70) {
+            Console.WriteLine("Congrats on passing the course!");
+        } else {
+            Console.WriteLine("Don't give up! There's always next time!");
+        }
     }
 }
