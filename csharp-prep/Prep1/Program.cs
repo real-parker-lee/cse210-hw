f58d594de6e2cb176eba32b844fde57ee5c6ff73
real-parker-lee <real-parker-lee@byui.edu>
2025-10-30 09:20:10 -0600

    Fixed parts of assignment I forgot to do.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Sandbox World!");
+        
+        string firstName;
+        string lastName;
+        
+        Console.WriteLine("Please enter your first name: ");
+        firstName = Console.ReadLine();
+        
+        Console.WriteLine("Please enter your last name: ");
+        lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}; {firstName} {lastName}.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        
+        string firstName;
+        string lastName;
+        
+        Console.WriteLine("Please enter your first name: ");
+        firstName = Console.ReadLine();
+        
+        Console.WriteLine("Please enter your last name: ");
+        lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}; {firstName} {lastName}.");
     }
 }
