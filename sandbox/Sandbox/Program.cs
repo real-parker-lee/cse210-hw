cfbf73cbb0cec85534952066cf0145ce9365dd22
real-parker-lee <real-parker-lee@byui.edu>
2025-11-03 09:59:22 -0700

    Lecture: Lists

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,42 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+//         Console.WriteLine("Hello Sandbox World!");
+//         
+//         string firstName;
+//         string lastName;
+//         
+//         Console.Write("Please enter your first name: ");
+//         firstName = Console.ReadLine();
+//         
+//         Console.Write("Please enter your last name: ");
+//         lastName = Console.ReadLine();
+//         
+//         Console.WriteLine($"Your name is {lastName}; {firstName} {lastName}.");
+//         
+//         int x = 10;
+//         Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+//         Console.WriteLine(Math.Pow(x, 10));
+//         
+//         // for loop from 0 - 1000 by tens
+//         for(int i = 0; i <= 1000; i+=10)
+//         {
+//             Console.WriteLine($"{i}");
+//         }
+//         
+//         bool done = false;
+//         // do-while loop: condition checked at the End of the loop.
+//         do
+//         {
+//             Console.Write("input age: ");
+//             // if you're worried about performance, avoid declaring variables within loops.
+//             int age = int.Parse(Console.ReadLine());
+//             
+//             if (age >= 0 && age <= 125)
+//             {
+//                 done = true;
+//                 Console.WriteLine($"Super age: {age}");
+//             }
+//         } while (!done);
         
-        string firstName;
-        string lastName;
+        // LISTS
+        // List is a class/type.
+        // lists are dynamic
+        List<int> numbers = new List<int>();
+        // adding elements
+        numbers.Add(10);
+        numbers.Add(3);
+        numbers.Add(20);
         
-        Console.Write("Please enter your first name: ");
-        firstName = Console.ReadLine();
-        
-        Console.Write("Please enter your last name: ");
-        lastName = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {lastName}; {firstName} {lastName}.");
-        
-        int x = 10;
-        Console.WriteLine($"{x}, {++x}, {x++}, {x}");
-        Console.WriteLine(Math.Pow(x, 10));
-        
-        // for loop from 0 - 1000 by tens
-        for(int i = 0; i <= 1000; i+=10)
+        // iterating over lists
+        foreach(int n in numbers)
         {
-            Console.WriteLine($"{i}");
+            Console.WriteLine($"number is {n}");
         }
         
-        bool done = false;
-        // do-while loop: condition checked at the End of the loop.
-        do
-        {
-            Console.Write("input age: ");
-            // if you're worried about performance, avoid declaring variables within loops.
-            int age = int.Parse(Console.ReadLine());
-            
-            if (age >= 0 && age <= 125)
-            {
-                done = true;
-                Console.WriteLine($"Super age: {age}");
-            }
-        } while (!done);
+        
     }
 }
