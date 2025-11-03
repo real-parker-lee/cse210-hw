a7c023da99b4834d65ac73d975fff1e6b79daed3
real-parker-lee <real-parker-lee@byui.edu>
2025-11-03 09:41:23 -0700

    Lecture: for loop practice

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -9,12 +9,22 @@ class Program
         string firstName;
         string lastName;
         
-        Console.WriteLine("Please enter your first name: ");
+        Console.Write("Please enter your first name: ");
         firstName = Console.ReadLine();
         
-        Console.WriteLine("Please enter your last name: ");
+        Console.Write("Please enter your last name: ");
         lastName = Console.ReadLine();
         
         Console.WriteLine($"Your name is {lastName}; {firstName} {lastName}.");
+        
+        int x = 10;
+        Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+        Console.WriteLine(Math.Pow(x, 10));
+        
+        // for loop from 0 - 1000 by tens
+        for(int i = 0; i <= 1000; i+=10)
+        {
+            Console.WriteLine($"{i}");
+        }
     }
 }
