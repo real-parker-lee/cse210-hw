8f3a913b9b91eb00fb5b10227ae3120e108a0b4d
real-parker-lee <real-parker-lee@byui.edu>
2025-11-04 14:42:38 -0700

    prep3: stretch chalenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,30 +7,68 @@ class Program
         /// generate a random int from 1 to 11
         Random randomGenerator = new Random();
         
-        int magicNum = randomGenerator.Next(1,11);
+        int magicNum;
+        
+        int guessCount = 0;
+        
+        bool playAgain = true;
+        bool responseValid = false;
         
         // Console.Write("Please enter your number: ");
         
         // int magicNum = int.Parse(Console.ReadLine());
         
         int guess;
-        do {
-            Console.Write("Guess the magic Number: ");
-            guess = int.Parse(Console.ReadLine());
-            
-            // guess check logic
-            if (guess > magicNum)
+        string replayStr = "";
+        do 
+        {
+            // game stuff
+            magicNum= randomGenerator.Next(1,11);
+            do {
+                Console.Write("Guess the magic Number: ");
+                guess = int.Parse(Console.ReadLine());
+                
+                // guess check logic
+                if (guess > magicNum)
+                {
+                    Console.WriteLine($"{guess} is too high! Guess lower.");
+                    ++guessCount;
+                }
+                else if (guess < magicNum)
+                {
+                    Console.WriteLine($"{guess} is too low! Guess higher.");
+                    ++guessCount;
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {++guessCount} tries! Good job!");
+                }
+            } while (guess != magicNum);
+            // menu stuff
+            do
             {
-                Console.WriteLine($"{guess} is too high! Guess lower.");
-            }
-            else if (guess < magicNum)
-            {
-                Console.WriteLine($"{guess} is too low! Guess higher.");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it! Good job!");
-            }
-        } while (guess != magicNum);
+                Console.Write("Play Again? [Y/n]: ");
+                replayStr = Console.ReadLine();
+                
+                if (replayStr == "" || replayStr == "y" || replayStr == "Y")
+                {
+                    playAgain = true;
+                    responseValid = true;
+                    guessCount = 0;
+                }
+                else if (replayStr == "n" || replayStr == "N")
+                {
+                    playAgain = false;
+                    responseValid = true;
+                    guessCount = 0;
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid input: '{replayStr}'. Choose either 'y' or 'n'");
+                    responseValid = false;
+                }
+            } while (!responseValid);
+        } while (playAgain);
+        
     }
 }
