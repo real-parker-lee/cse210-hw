c967c0fd5bdc95d82edc57929caf0e97b94fadfe
real-parker-lee <real-parker-lee@byui.edu>
2025-11-04 09:57:12 -0700

    Lecture: Class Objects

--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,16 @@
 {
   static void Main()
   {
-    Console.WriteLine("Bonjour toute le monde");
+    // Console.WriteLine("Bonjour toute le monde");
+    Planet mercury = new Planet();
+    mercury._name = "Mercury";
+    mercury._diameter = 1234.343;
+    
+    Planet venus = new Planet();
+    venus._name = "Venus";
+    venus._diameter = 2345.67;
+    
+    mercury.DisplayPlanetInformation();
+    venus.DisplayPlanetInformation();
   }
 }
