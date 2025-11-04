6ddcc389ce58abab66c47571ae2440a37702699b
real-parker-lee <real-parker-lee@byui.edu>
2025-11-04 14:22:35 -0700

    Prep3: Step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // ask user for number
+        Console.Write("Please enter your number: ");
+        
+        int magicNum = int.Parse(Console.ReadLine());
+        
+        // ask for a guess
+        Console.Write("Guess the magic Number: ");
+        int guess = int.Parse(Console.ReadLine());
+        
+        // guess check logic
+        if (guess > magicNum)
+        {
+            Console.WriteLine($"{guess} is too high! Guess lower.");
+        }
+        else if (guess < magicNum)
+        {
+            Console.WriteLine($"{guess} is too low! Guess higher.");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it! Good job!");
+        }
     }
 }
