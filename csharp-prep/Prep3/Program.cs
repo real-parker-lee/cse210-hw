d2f8033281ebf7049bbdf38da4d02f14e4dde8c0
real-parker-lee <real-parker-lee@byui.edu>
2025-11-04 14:29:45 -0700

    prep3 steps 2-3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,27 +4,33 @@ class Program
 {
     static void Main()
     {
-        // ask user for number
-        Console.Write("Please enter your number: ");
+        /// generate a random int from 1 to 11
+        Random randomGenerator = new Random();
         
-        int magicNum = int.Parse(Console.ReadLine());
+        int magicNum = randomGenerator.Next(1,11);
         
-        // ask for a guess
-        Console.Write("Guess the magic Number: ");
-        int guess = int.Parse(Console.ReadLine());
+        // Console.Write("Please enter your number: ");
         
-        // guess check logic
-        if (guess > magicNum)
-        {
-            Console.WriteLine($"{guess} is too high! Guess lower.");
-        }
-        else if (guess < magicNum)
-        {
-            Console.WriteLine($"{guess} is too low! Guess higher.");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it! Good job!");
-        }
+        // int magicNum = int.Parse(Console.ReadLine());
+        
+        int guess;
+        do {
+            Console.Write("Guess the magic Number: ");
+            guess = int.Parse(Console.ReadLine());
+            
+            // guess check logic
+            if (guess > magicNum)
+            {
+                Console.WriteLine($"{guess} is too high! Guess lower.");
+            }
+            else if (guess < magicNum)
+            {
+                Console.WriteLine($"{guess} is too low! Guess higher.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! Good job!");
+            }
+        } while (guess != magicNum);
     }
 }
