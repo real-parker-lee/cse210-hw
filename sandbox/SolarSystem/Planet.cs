8e3792dbd036372c756019a44ade991a7bfc2778
real-parker-lee <real-parker-lee@byui.edu>
2025-11-04 09:53:38 -0700

    Lecture: Classes

---
+++ sandbox/SolarSystem/Planet.cs
@@ -0,0 +1,11 @@
+class Planet
+{
+  public string _name;
+  public double _diameter;
+  
+  public void DisplayPlanetInformation()
+  {
+    Console.WriteLine($"NAME: {_name}\nDIAM: {_diameter}");
+  }
+  
+}
---
+++ sandbox/SolarSystem/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("Bonjour toute le monde");
+  }
+}
