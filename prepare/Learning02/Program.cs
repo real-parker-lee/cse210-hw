af479210120a6fd21d4cf20bda3c38d1d24e2d7e
real-parker-lee <real-parker-lee@byui.edu>
2025-11-05 17:37:09 -0700

    Learning02: complete step 1-2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Instructor";
+        job1._company = "Mathnasium";
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Underwater Basket Weaver";
+        job2._company = "UBWAA";
+        job2._startYear = 1950;
+        job2._endYear = 2030;
+        
+        Console.WriteLine($"{job1._company}");
+        Console.WriteLine($"{job2._company}");
     }
 }
--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -7,15 +7,51 @@
     // Console.WriteLine("Bonjour toute le monde");
     Planet mercury = new Planet();
     mercury._name = "Mercury";
-    mercury._diameter = 1234.343;
+    mercury._diameter = 4880.00;
     
     Planet venus = new Planet();
     venus._name = "Venus";
-    venus._diameter = 2345.67;
+    venus._diameter = 12104.00;
+    
+    Planet earth = new Planet();
+    earth._name = "Earth";
+    earth._diameter = 12742.00;
+    
+    Planet mars = new Planet();
+    mars._name = "Mars";
+    mars._diameter = 6779.00;
+    
+    Planet jupiter = new Planet();
+    jupiter._name = "Jupiter";
+    jupiter._diameter = 139822.00;
+    
+    Planet saturn = new Planet();
+    saturn._name = "Saturn";
+    saturn._diameter = 116464.00;
+    
+    Planet uranus = new Planet();
+    uranus._name = "Uranus";
+    uranus._diameter = 50724.00;
+    
+    Planet neptune = new Planet();
+    neptune._name = "Neptune";
+    neptune._diameter = 49244.00;
+    
+    Planet pluto = new Planet();
+    pluto._name = "Pluto";
+    pluto._diameter = 2376.00;
     
     solarSystem._solarSystem.Add(mercury);
+    solarSystem._solarSystem.Add(venus);
+    solarSystem._solarSystem.Add(earth);
+    solarSystem._solarSystem.Add(mars);
+    solarSystem._solarSystem.Add(jupiter);
+    solarSystem._solarSystem.Add(saturn);
+    solarSystem._solarSystem.Add(uranus);
+    solarSystem._solarSystem.Add(neptune);
+    solarSystem._solarSystem.Add(pluto);
     
-    mercury.DisplayPlanetInformation();
-    venus.DisplayPlanetInformation();
+    // mercury.DisplayPlanetInformation();
+    // venus.DisplayPlanetInformation();
   }
 }
