7d0639f30ce18de19efbe2926bd0ccabdad4f045
real-parker-lee <real-parker-lee@byui.edu>
2025-11-06 09:44:38 -0700

    initial menu implementation

---
+++ prove/Develop02/Menu.cs
@@ -0,0 +1,21 @@
+class Menu
+{
+  public string[] _menuStrings = {
+    "Welcome to the Count",
+    "OPTIONS:",
+    "1. Create Journal Entry",
+    "2. Display Journal",
+    "3. Save Journal to File",
+    "4. Read Journal from File",
+    "5. Quit"
+  };
+  
+  public void ProcessMenu()
+  {
+    foreach (string s in _menuStrings)
+    {
+      Console.WriteLine(s);
+    }
+    
+  }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Menu menu = new Menu();
+        menu.ProcessMenu();
     }
 }
