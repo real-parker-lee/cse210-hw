00d900dd582675f7839e1e66b8693eb9206c9aec
real-parker-lee <real-parker-lee@byui.edu>
2025-11-11 09:45:08 -0700

    Lecture: encapsulation example

---
+++ sandbox/CircleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Hello CircleApp World!")
+    Circle circ = new Circle();
+    
+    //circ._radius = 10;
+    circ.SetRadius(10);
+    
+    Console.WriteLine(circ.GetCircleArea());
+  }
+}
