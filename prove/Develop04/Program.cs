6e67454b6964723a843780e19d2df16f47ffda3d
real-parker-lee <real-parker-lee@byui.edu>
2025-11-19 18:19:01 -0700

    Develop04: implement reflection activity

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,8 +7,25 @@ class Program
         Breathing b = new Breathing(4, 6);
         b.SetDuration(20);
         
+        Reflection r = new Reflection();
+        r.AddPrompt("Think of a time when you stood up for someone else...");
+        r.AddPrompt("Think of a time when you did something really difficult...");
+        r.AddPrompt("Think of a time when you helped someone in need...");
+        r.AddPrompt("Think of a time when you did something truly selfless...");
+        
+        r.AddQuestion("Why was this experience meaningful to you?");
+        r.AddQuestion("Have you ever done anything like this before?");
+        r.AddQuestion("How did you get started?");
+        r.AddQuestion("How did you feel when it was complete?");
+        r.AddQuestion("What made this time different than other times when you were not as successful?");
+        r.AddQuestion("What is your favorite thing about this experience?");
+        r.AddQuestion("What could you learn from this experience that applies to other situations?");
+        r.AddQuestion("What did you learn about yourself through this experience?");
+        r.AddQuestion("How can you keep this experience in mind in the future?");
+        
         //mainMenu.AddItem(new MenuItem("Test Activity class Functions", a));
         mainMenu.AddItem(new MenuItem("Practice Mindful Breathing", b));
+        mainMenu.AddItem(new MenuItem("Reflect on Resillience", r));
         
         Activity chosen = mainMenu.Show();
         chosen.Run();
---
+++ prove/Develop04/Reflection.cs
@@ -0,0 +1,66 @@
+public class Reflection : Activity
+{
+  private List<string> _prompts = new List<string>();
+  private List<string> _questions = new List<string>();
+  private int _secondsPerQuestion = 5;
+  
+  public Reflection()
+  : base()
+  {
+    SetDescription("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+  }
+  
+  public void AddPrompt(string prompt)
+  {
+    _prompts.Add(prompt);
+  }
+  
+  public void AddQuestion(string question)
+  {
+    _questions.Add(question);
+  }
+  
+  public List<string> GetPrompts()
+  {
+    return _prompts;
+  }
+  
+  public List<string> GetQuestions()
+  {
+    return _questions;
+  }
+  
+  public string GetRandomPrompt()
+  {
+    Random rand = new Random();
+    return GetPrompts()[rand.Next(0, GetPrompts().Count)];
+  }
+  
+  public string GetRandomQuestion()
+  {
+    Random rand = new Random();
+    return GetQuestions()[rand.Next(0,GetQuestions().Count)];
+  }
+  
+  public override void Run()
+  {
+    // display common welcome.
+    Console.WriteLine($"{GetStartMessage()}\n");
+    Console.WriteLine($"{GetDescription()}\n");
+    SetDuration(PromptDuration());
+    Console.Write($"{GetRandomPrompt()} ");
+    SetTimerPos();
+    DisplayTimer(3);
+    Console.WriteLine("\n");
+    // begin questions 
+    for (int i = 0; i < GetDuration() / _secondsPerQuestion; i++)
+    {
+      Console.Write($"{GetRandomPrompt()} ");
+      SetThrobberPos();
+      DisplayThrobber(_secondsPerQuestion * 1000, 250);
+      Console.WriteLine("\n");
+    }
+    
+    Console.WriteLine(GetEndMessage());
+  }
+}
