8f17c99a19791dd2503f1ea34c6dcab79828a5ee
real-parker-lee <real-parker-lee@byui.edu>
2025-12-03 15:57:14 -0700

    Develop05: implement about, clear. Added feedback messages after commands.

--- prove/Develop05/GoalTracker.cs
+++ prove/Develop05/GoalTracker.cs
@@ -125,7 +125,6 @@ public class GoalTracker
     {
       case "help":
         Console.WriteLine("-----COMMAND-----   ------------DESCRIPTION------------");
-        Console.WriteLine("");
         Console.WriteLine("load [path]         Load data from file at given path.");
         Console.WriteLine("save [?path]        Save data to file at given path.");
         Console.WriteLine("                      If a path is not specified, will");
@@ -139,8 +138,12 @@ public class GoalTracker
         Console.WriteLine("new-checklist       Create a new checklist goal");
         Console.WriteLine("new one-time        Create a new one-time goal");
         Console.WriteLine("quit                Exit the application.");
+        Console.WriteLine("clear               Clear the screen.");
         Console.WriteLine("help                Show this help message.");
+        Console.WriteLine("about               Show the program description.");
+        Console.WriteLine("");
         break;
+        
       case "save":
         if (args.Count() == 1 && GetCurrentPath() == "")
         {
@@ -152,9 +155,10 @@ public class GoalTracker
           SetCurrentPath(args[1]);
         }
         SerializeAll();
+        Console.WriteLine($"Saved {GetGoals().Count()} goals to file at \"{GetCurrentPath()}\"\n");
         break;
+        
       case "load":
-        Console.Write("Load from Path: ");
         if (args.Length == 1 && GetCurrentPath() == "")
         {
           Console.Write("Choose a path: ");
@@ -165,11 +169,13 @@ public class GoalTracker
           SetCurrentPath(args[1]);
         }
         Deserialize();
-        Console.WriteLine($"Loaded {GetGoals().Count()} goals from {GetCurrentPath()}\n");
+        Console.WriteLine($"Loaded {GetGoals().Count()} goals from file at \"{GetCurrentPath()}\"\n");
         break;
+        
       case "list":
         ShowAll();
         break;
+        
       case "check-off":
         int idx = 0;
         if (args.Length < 2)
@@ -182,31 +188,62 @@ public class GoalTracker
         {
           idx = int.Parse(args[1]) - 1;
         }
-        
         BaseGoal g = SelectGoal(idx);
         int deltapts = g.CheckOff();
         SetPoints(GetPoints() + deltapts);
+        Console.WriteLine($"Checked off goal #{idx + 1}.\n");
         break;
+        
       case "points":
-        Console.WriteLine($"You have {GetPoints()} points.");
+        Console.WriteLine($"You have {GetPoints()} points.\n");
         break;
+        
       case "new-eternal":
         break;
+        
       case "new-checklist":
         break;
+        
       case "new-onetime":
         break;
+        
       case "quit":
         SetShouldExit(true);
+        Console.WriteLine("Goodbye.\n");
         break;
+        
+      case "about":
+        Console.WriteLine("EternalQuest is a program designed to track and gamify your goals, both short and long-term.");
+        Console.WriteLine("");
+        Console.WriteLine("There are three types of goals:");
+        Console.WriteLine("1. One-Time Goals: goals meant to be accomplished once.");
+        Console.WriteLine("2. Check-list Goals: Goals meant to be done a set number of times (or more if you want).");
+        Console.WriteLine("3. Eternal Goals: Goals you try to always do.");
+        Console.WriteLine("");
+        Console.WriteLine("Each goal gives points when you check it off, but Check-list goals will give a bonus once you reach the set threshold.");
+        Console.WriteLine("");
+        Console.WriteLine("You write commands to interact with the program. To create a new Eternal goal, you'd write:\n");
+        Console.WriteLine("new-eternal my-goal 1000");
+        Console.WriteLine("\nto make a new eternal goal named \"my-goal\" worth 1000 points.");
+        Console.WriteLine("Alternatively, you could run\n");
+        Console.WriteLine("new-eternal");
+        Console.WriteLine("\nto enter a more guided interactive environment, which will prompt you for the values you want.");
+        Console.WriteLine("\nFor a list of all commands, run the \"help\" command.\n");
+        break;
+        
+      case "clear":
+        Console.Clear();
+        break;
+        
       default:
-        Console.WriteLine($"Invalid Command: \"{cmd}\"");
+        Console.WriteLine($"Invalid Command: \"{cmd}\"\n");
         break;
     }
   }
   
   public void RunRepl()
   {
+    Console.Clear();
     do
     {
       ReplStep();
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,30 +1,8 @@
 public class Program
 {
     static void Main(string[] args)
-    {
-//         List<BaseGoal> goals = new List<BaseGoal>();
-//         goals.Add(new EternalGoal("test goal", 1000));
-//         goals.Add(new OneTimeGoal("onetime", 2000));
-//         goals.Add(new ChecklistGoal("checklist", 1500, 500, 5));
-//         
-//         for (int i = 0; i < goals.Count; i++)
-//         {
-//             Console.WriteLine($"{i+1}. {goals[i].Show()}");
-//         }
-//         int points = 0;
-//         Console.WriteLine(points);
-//         points = goals[0].CheckOff();
-//         Console.WriteLine(points);
-//         
-//         for (int i = 0; i < goals.Count; i++)
-//         {
-//             Console.WriteLine($"{i+1}. {goals[i].Show()}");
-//         }
+    {   
         GoalTracker tracker = new GoalTracker();
-        // TEST: Read from File
-        // tracker.Deserialize();
-        // tracker.ShowAll();
-        
         tracker.RunRepl();
     }
 }
